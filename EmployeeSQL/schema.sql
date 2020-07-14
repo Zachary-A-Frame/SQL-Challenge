1dc40ed42dfc81e9e6bb74858d19bd0f1f57e54c
Zach <Zacaframe@gmail.com>
2020-07-14 01:03:07 -0400

    Adding screenshots and more queries.

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -82,6 +82,28 @@ INNER JOIN Departments ON Department_Employees.dept_no = Departments.dept_no);
 SELECT first_name, last_name, sex FROM Employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT Employees.emp_no, Employees.last_name, Employees.first_name, Departments.dept_name
+FROM ((Employees
+INNER JOIN Department_Employees ON Employees.emp_no = Department_Employees.emp_no)
+INNER JOIN Departments ON Department_Employees.dept_no = Departments.dept_no)
+WHERE dept_name = 'Sales';
+
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT Employees.emp_no, Employees.last_name, Employees.first_name, Departments.dept_name
+FROM ((Employees
+INNER JOIN Department_Employees ON Employees.emp_no = Department_Employees.emp_no)
+INNER JOIN Departments ON Department_Employees.dept_no = Departments.dept_no)
+WHERE dept_name = 'Sales' OR dept_name = 'Development';
+
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name) FROM Employees
+GROUP BY last_name
+ORDER BY count(last_name) DESC
+
+
+
+
 
 
 
