e8d6ecffaaa7c9c77d16d6cde975b497d71fda88
Zach <Zacaframe@gmail.com>
2020-07-14 00:46:39 -0400

    Adding screenshots and more queries.

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,11 +1,3 @@
--- drop table Employees CASCADE;
--- drop table Departments CASCADE;
--- drop table deptartment_emp CASCADE;
--- drop table dept_manager CASCADE;
--- drop table Salaries CASCADE;
--- drop table Titles CASCADE;
-
-
 -- Creating Table for Employees.csv
 
 -- CREATE TABLE Employees (
@@ -66,21 +58,32 @@ SELECT * FROM Employees;
 SELECT Employees.emp_no, Employees.last_name, Employees.first_name, Employees.sex, Salaries.salary
 FROM Employees
 INNER JOIN Salaries ON 
-Employees.emp_no = Salaries.emp_no
+Employees.emp_no = Salaries.emp_no;
 
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
 
 SELECT first_name, last_name, hire_date FROM Employees
-WHERE hire_date >= '1985-12-31' AND Employees.hire_date < '1987-01-01'
+WHERE hire_date >= '1985-12-31' AND Employees.hire_date < '1987-01-01';
 
 -- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 
 SELECT Department_Manager.emp_no, Department_Manager.dept_no, Departments.dept_no,  Departments.dept_name, Employees.last_name, Employees.first_name 
 FROM ((Department_Manager
 	  INNER JOIN Departments ON Department_Manager.dept_no = Departments.dept_no)
-	  INNER JOIN Employees ON Department_Manager.emp_no = Employees.emp_no)
+	  INNER JOIN Employees ON Department_Manager.emp_no = Employees.emp_no);
 	  
--- 4. 
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT Employees.emp_no, Employees.last_name, Employees.first_name, Departments.dept_name 
+FROM ((Employees
+INNER JOIN Department_Employees ON Employees.emp_no = Department_Employees.emp_no)
+INNER JOIN Departments ON Department_Employees.dept_no = Departments.dept_no);
+
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex FROM Employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+
+
 
 
 
